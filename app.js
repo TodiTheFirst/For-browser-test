54876b25a8f436313bf193de66ed231d8af99435
TodiTheFirst <146870436+TodiTheFirst@users.noreply.github.com>
2023-10-08 12:25:15 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+function getName(){
+  return prompt('Your naming','')
+}
+
+function greet(){
+  const userName = getName();
+  console.log("Hello "+ userName);
+}
+
+greet();
+let object = { name : 'cringe'}; // мусорный коллектор
+
+object = "kkk";
+
+const addListenerBtn = document.getElementById('add-listener-btn');
+const clickableBtn = document.getElementById('clickable-btn');
+const messageInput = document.getElementById('click-message-input');
+
+function printMessage() {
+  const value = messageInput.value;
+  console.log(value || 'Clicked me!');
+}
+
+function addListener() {
+  clickableBtn.addEventListener('click', printMessage);
+}
+
+addListenerBtn.addEventListener('click', addListener);
